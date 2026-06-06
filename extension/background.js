e7012acb126db351a4f84bb83f5e653875dfd367
James Gabriel B. Boo <boojamesgabriel@gmail.com>
2026-06-06 22:05:51 +0800

    started the background file

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("LockIn extension installed and background service worker running.");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "saveState") {
+    chrome.storage.local.set(message.data, () => {
+      console.log("State saved:", message.data);
+      sendResponse({status: "ok"});
+    });
+    return true;
+  }
+
+  if (message.type === "loadState"){
+    chrome.storage.local.get([
+      "sessionDuration",
+      "sessionBreak",
+      "isQuickieMode",
+      "hasSelection",
+      "originalSessionDuration",
+      "isOnBreak"
+    ],(result)=>{
+      console.log("State loaded:", result);
+      sendResponse(result);
+    });
+    return true;
+  }
 });
+
